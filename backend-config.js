e8892750b17568474f5f31c5c3500cc6f17a95c0
aopikaaa <t.o.glasstea@gmail.com>
2026-03-15 23:54:13 +0900

    Add files via upload

--- backend-config.js
+++ backend-config.js
@@ -2,6 +2,6 @@ window.__BACKEND_CONFIG__ = {
   appsScriptUrl: "https://script.google.com/macros/s/AKfycbyOXip-f5pHv9HCCvc5GAtQ_UwWb8km6bafGlOpcWD1xNDlgwK6bnzZh5D5anwzZEEV/exec",
   apiToken: "splintersplintersplinter",
   defaultUserId: "",
-  userOptions: ["あおぴか","randam","りこ","あみ"],
+  userOptions: ["あおぴか", "randam", "りこ", "あみ", "さく", "准将", "たまてゃ", "itsuki", "おかかまる"],
   autoSync: false,
 };
